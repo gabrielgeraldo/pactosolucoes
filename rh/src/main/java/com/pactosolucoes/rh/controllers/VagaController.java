7b2d31f096b2b376891676284f16178e23c7e9a2
Gabriel Geraldo <gabrieldomingosgeraldo@gmail.com>
2024-12-25 17:31:53 -0300

    commit

--- rh/src/main/java/com/pactosolucoes/rh/controllers/VagaController.java
+++ rh/src/main/java/com/pactosolucoes/rh/controllers/VagaController.java
@@ -27,34 +27,31 @@ public class VagaController {
 	private VagaService vagaService;
 
 	@GetMapping
-	public ResponseEntity<List<VagaDTO>> buscar(
-			@RequestParam(value ="id" , required = false) Long id,
-			@RequestParam(value ="titulo" , required = false) String titulo,
-			@RequestParam(value = "descricao", required = false) String descricao
-			) {
-		
+	public ResponseEntity<List<VagaDTO>> buscar(@RequestParam(value = "id", required = false) Long id,
+			@RequestParam(value = "titulo", required = false) String titulo,
+			@RequestParam(value = "descricao", required = false) String descricao) {
+
 		VagaDTO vagaFiltroDTO = new VagaDTO(id, titulo, descricao);
-		
+
 		List<VagaDTO> vagasDTO = vagaService.buscar(vagaFiltroDTO);
 		return ResponseEntity.ok(vagasDTO);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity salvar( @RequestBody VagaDTO vagaDTO ) {
+	public ResponseEntity salvar(@RequestBody VagaDTO vagaDTO) {
 		try {
 			vagaDTO = vagaService.salvar(vagaDTO);
 			return new ResponseEntity(vagaDTO, HttpStatus.CREATED);
-		}catch (RegraNegocioException e) {
+		} catch (RegraNegocioException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
-	
+
 	@PutMapping("{id}")
 	public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody VagaDTO vagaDTO) {
 		return vagaService.obterPorId(id).map(entity -> {
 			try {
 				vagaDTO.setId(entity.getId());
-				System.out.println(entity.getId());
 				vagaService.atualizar(vagaDTO);
 				return ResponseEntity.ok(vagaDTO);
 			} catch (RegraNegocioException e) {
@@ -62,12 +59,23 @@ public class VagaController {
 			}
 		}).orElseGet(() -> new ResponseEntity("Vaga não encontrada na base de Dados.", HttpStatus.BAD_REQUEST));
 	}
-	
+
 	@DeleteMapping("{id}")
 	public ResponseEntity deletar(@PathVariable("id") Long id) {
 		return vagaService.obterPorId(id).map(entidade -> {
 			vagaService.deletar(entidade);
 			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}).orElseGet(() -> new ResponseEntity("Vaga não encontrada na base de Dados.", HttpStatus.BAD_REQUEST));
+	}
+
+	@GetMapping("{id}")
+	public ResponseEntity obterPorId(@PathVariable("id") Long id) {
+		return vagaService.obterPorId(id).map(entity -> {
+			try {
+				return ResponseEntity.ok(vagaService.obterPorId(id));
+			} catch (RegraNegocioException e) {
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
 		}).orElseGet(() -> new ResponseEntity("Vaga não encontrada na base de Dados.", HttpStatus.BAD_REQUEST));
 	}
 }
--- rh/src/main/java/com/pactosolucoes/rh/domain/Usuario.java
+++ rh/src/main/java/com/pactosolucoes/rh/domain/Usuario.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -16,31 +17,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name="usuario")
-@Table(name="usuario")
+@Entity(name = "usuario")
+@Table(name = "usuario", uniqueConstraints = @UniqueConstraint(columnNames = "email", name = "unique_email"))
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(of="id") 
+@EqualsAndHashCode(of = "id")
 public class Usuario {
 
 	@Id
 	@Column(name = "id")
-	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "nome")
 	@NotNull
 	private String nome;
-	
+
 	@Column(name = "email")
 	@NotNull
 	private String email;
-	
+
 	@Column(name = "senha")
 	@NotNull
 	@JsonIgnore
 	private String senha;
-	
+
 }
